6a2f4390c8266fa85ed30042642ec797cf73f5e5
m-silver <mattsilver@windowslive.com>
2020-08-14 11:10:16 +1200

    Add working route root with home layout

--- routes.js
+++ routes.js
@@ -6,3 +6,14 @@ module.exports = router
 
 // routes
 
+router.get('/', (req, res) => {
+  // get the data
+  utils.getData((err, characters) => {
+    if (err) {
+      res.status(500).send('Oh no! 500 Error!')
+    } else {
+    // render the template
+    res.render('home', characters)
+    }
+  })
+})
--- utils.js
+++ utils.js
@@ -1,8 +1,48 @@
 const fs = require('fs')
 const path = require('path')
 
+const filepath = path.join(__dirname, 'data.json')
+
 module.exports = {
   getData
 }
 
 // data functions
+
+function getData (callback) {
+  // read the file
+  fs.readFile(filepath, 'utf8', (err, contents) => {
+    if (err) {
+      callback(new Error(err.message))
+    } else {
+    // parse the contents
+    const characters = JSON.parse(contents)
+    // call the callback
+    callback(null, characters)
+    }
+  })
+}
+
+// function getDinoData (callback) {
+//   // read the file
+//   fs.readFile(filepath, 'utf8', (err, contents) => {
+//     // parse the contents
+//     const dinos = JSON.parse(contents)
+//     // call the callback
+//     callback(null, dinos)
+//   })
+// }
+
+// function getMarvelData(callback) {
+//   const filepath = path.join(__dirname, 'data/marvel.json')
+//   fs.readFile(filepath, 'utf8', (err, contents) => {
+//     if (err) {
+//       // call our callback with an error
+//       callback(new Error(err.message))
+//     } else {
+//       // turn the contents into a js object
+//       const marvelData = JSON.parse(contents)
+//       callback(null, marvelData)
+//     }
+//   })
+// }
